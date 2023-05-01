b66e0d39846a382ed27bc180824647c61d7dcda8
dohaseif <dohaseif2@gmail.com>
2023-05-02 00:23:04 +0200

    extends

---
+++ Extends/resources/views/example.blade.php
@@ -0,0 +1,7 @@
+@extends('tamplete')
+@section('content')
+    <h2>second</h2>
+@endsection
+@section('forth')
+    <h2>forth</h2>
+@endsection
---
+++ Extends/resources/views/tamplete.blade.php
@@ -0,0 +1,8 @@
+<h1>
+    first
+</h1>
+@yield('content')
+<h2>
+    third
+</h2>
+@yield('forth')
